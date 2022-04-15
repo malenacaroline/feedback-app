521427725dc405efd8da1c44d9ba770f0974a4f5
Malena Aguiar <malenacaroline01@gmail.com>
2022-04-15 20:00:22 -0300

    feat: add data and methods from backend

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,15 +1,19 @@
 import React, { useContext } from "react";
 import FeedbackItem from "./FeedbackItem";
+import Spinner from "./shared/Spinner";
 import { motion, AnimatePresence } from "framer-motion";
 import FeedbackContext from "../context/FeedbackContext";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet!</p>;
   }
-  return (
+
+  return isLoading ? (
+    <Spinner/>
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
@@ -19,10 +23,7 @@ function FeedbackList() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <FeedbackItem
-              key={item.id}
-              item={item}
-            />
+            <FeedbackItem key={item.id} item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
